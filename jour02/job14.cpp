7b5f3f5dbffc75a867a1a51c82c6aff6454e2b2a
Tarek <tarek.adam@laplateforme.io>
2024-05-02 16:19:37 +0200

    jobs 14

--- jour02/job14.cpp
+++ jour02/job14.cpp
@@ -36,3 +36,9 @@ int main(){
 
 return 0;
 }
+
+// Tous les entiers de 1 à 9 sont narcissiques.
+// Les dix termes suivants de la suite des 88 nombres narcissiques (suite A005188 de l'OEIS) sont 153, 370, 371, 407, 1 634, 8 208, 9 474, 54 748, 92 727 et 93 084.
+
+//     153 = 1 3 + 5 3 + 3 3 {\displaystyle 153=1^{3}+5^{3}+3^{3}}.
+//     93084 = 9 5 + 3 5 + 0 5 + 8 5 + 4 5 {\displaystyle 93084=9^{5}+3^{5}+0^{5}+8^{5}+4^{5}}.
